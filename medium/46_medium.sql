b53931ea0a4ee550600d461ef94334d29fc1a1d7
Apurva Shekhar <apurva.shekhar@thoughtspot.com>
2020-10-21 15:22:57 -0700

    Get Highest Answer Rate Question

---
+++ medium/46_medium.sql
@@ -0,0 +1,12 @@
+Q- Get Highest Answer Rate Question
+SELECT question_id as survey_log
+FROM
+(
+	SELECT question_id,
+         SUM(case when action="answer" THEN 1 ELSE 0 END) as num_answer,
+        SUM(case when action="show" THEN 1 ELSE 0 END) as num_show
+	FROM survey_log
+	GROUP BY question_id
+) as tbl
+ORDER BY (num_answer / num_show) DESC
+LIMIT 1
