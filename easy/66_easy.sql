10eaa7058c6e3146fe876144c472aedb573336c1
Apurva Shekhar <apurva.shekhar@thoughtspot.com>
2020-12-13 16:45:33 -0800

    Product's Worth Over Invoices

---
+++ easy/66_easy.sql
@@ -0,0 +1,10 @@
+Q- Product's Worth Over Invoices
+select name,
+sum(rest) as rest,
+sum(paid) as paid,
+sum(canceled) as canceled,
+sum(refunded) as refunded
+from Product p, Invoice i
+where p.product_id = i.product_id
+group by p.name
+order by name
