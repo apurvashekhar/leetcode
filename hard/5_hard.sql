c382472ab742651e74a5e79cf80789e73599f14a
Apurva Shekhar <apurva.shekhar@thoughtspot.com>
2021-01-03 09:03:45 -0800

    Get the Second Most Recent Activity

--- hard/5_hard.sql
+++ hard/5_hard.sql
@@ -1,7 +1,8 @@
 Q- Get the Second Most Recent Activity
 select USERNAME, ACTIVITY, STARTDATE, ENDDATE
 from (
-    select username, activity, startDate, endDate,rnk, max(rnk) over(partition by username) as                  max_rnk, count(*) over (partition by username) as cnt
+    select username, activity, startDate, endDate,rnk, max(rnk) over(partition by username) as max_rnk,
+    count(*) over (partition by username) as cnt
     from(
         select *, rank() over (partition by username order by startDate, endDate) as rnk
         from UserActivity) t1) t2
