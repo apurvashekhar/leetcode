b07f70fb949c3727710391cea4a4cbf54a6db67f
Apurva Shekhar <37127372+apurvashekhar@users.noreply.github.com>
2021-12-14 17:13:14 -0800

    Delete 09.py

--- python/easy/09.py
+++
@@ -1,6 +0,0 @@
-Q- Defanging an IP Address.
-class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        split_str = address.split(".")
-        ans = "[.]".join(split_str)
-        return ans
